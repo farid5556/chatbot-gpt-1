99c3b803ecd4989a3d43d61b76acf2e309f03e01
Farid Hasfindra <farid06hasfindra@gmail.com>
2025-05-30 20:01:49 +0700

    a

---
+++ app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+from retriever import get_vectorstore
+from generator import generate_answer
+
+app = FastAPI()
+vectorstore = get_vectorstore()
+
+class Question(BaseModel):
+    query: str
+
+@app.post("/ask")
+async def ask_question(question: Question):
+    docs = vectorstore.similarity_search(question.query, k=2)
+    context = " ".join([doc.page_content for doc in docs])
+    answer = generate_answer(context, question.query)
+    return {"answer": answer}
+
+# http://127.0.0.1:8000/ask
